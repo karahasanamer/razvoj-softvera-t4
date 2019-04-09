2f246d2a73cc7e2ebf11f0f09540f1b505726dc0
karahasanamer <akarahasan4@etf.unsa.ba>
2019-04-09 11:49:01 +0200

    Dodan izgled

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,21 @@
 package sample;
+import java.util.ArrayList;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
 
 public class Controller {
 
 
+    public TextArea unosArtikala;
+    public TextArea ispisArtikala;
+
+    public void dodajArtikle(ActionEvent actionEvent) {
 
 
 
+
+
+
+    }
 }
