c37b58b66fed0244c1786b03d469e40e21b69a1f
karahasanamer <akarahasan4@etf.unsa.ba>
2019-04-10 09:57:28 +0200

    Funkcionalnost dodavanja

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,10 +9,24 @@ public class Controller {
 
     public TextArea unosArtikala;
     public TextArea ispisArtikala;
-
     public void dodajArtikle(ActionEvent actionEvent) {
 
+        ArrayList<Artikal> a = new ArrayList<Artikal>();
 
+        String[] str = unosArtikala.getText().split("\n");
+
+   for (String i : str) {
+        a.add(new Artikal(i));
+
+   }
+
+   Artikal.izbaciDuplikate(a);
+
+   String c ="";
+   for(Artikal i : a){
+       c = c + i + "\n";
+   }
+   ispisArtikala.setText(c);
 
 
 
